88f75348d03322ea5b409fb8347f8fd1d194d223
Spencer Truett <srtruett1993@gmail.com>
2021-03-08 11:41:44 -0600

    added basic form validation

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -2,11 +2,33 @@
   <div>
     <h3 class="teal--text">New Contact</h3>
     <v-form @submit.prevent="handleSubmit">
-      <v-text-field outlined label="First Name" v-model="form.firstName" />
-      <v-text-field outlined label="Last Name" v-model="form.lastName" />
-      <v-text-field type="number" outlined label="Phone" v-model="form.phone" />
-      <v-select outlined label="Phone Type" :items="phoneTypeOptions" v-model="form.type" />
-      <v-text-field outlined label="Email" v-model="form.email" />
+      <v-text-field
+        outlined
+        label="First Name"
+        v-model="form.firstName"
+        :rules="validators.firstName"
+      />
+      <v-text-field
+        outlined
+        label="Last Name"
+        v-model="form.lastName"
+        :rules="validators.lastName"
+      />
+      <v-text-field
+        type="number"
+        outlined
+        label="Phone"
+        v-model="form.phone"
+        :rules="validators.phone"
+      />
+      <v-select
+        outlined
+        label="Phone Type"
+        :items="phoneTypeOptions"
+        v-model="form.type"
+        :rules="validators.type"
+      />
+      <v-text-field outlined label="Email" v-model="form.email" :rules="validators.email" />
       <v-btn type="submit" color="teal" dark>Submit</v-btn>
     </v-form>
   </div>
@@ -23,7 +45,26 @@ export default {
         type: "",
         email: ""
       },
-      phoneTypeOptions: ["Home", "Cell", "Office"]
+      phoneTypeOptions: ["Home", "Cell", "Office"],
+      validators: {
+        firstName: [
+          val => !!val || "Contact first name is required",
+          val => val.length < 25 || "First name must be less than 25 characters"
+        ],
+        lastName: [
+          val => !!val || "Contact last name is required",
+          val => val.length < 25 || "Last name must be less than 25 characters"
+        ],
+        phone: [
+          val => !!val || "Phone number is required",
+          val => val.length === 10 || "Phone number must be 10 characters"
+        ],
+        type: [val => !!val || "Phone type is required"],
+        email: [
+          val => !!val || "Email is required",
+          val => val.includes("@") || "Please enter a valid email"
+        ]
+      }
     };
   },
   methods: {
